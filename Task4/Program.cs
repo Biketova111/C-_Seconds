e66f89116cfc25e5702f9a7f2b2011b73f28215b
Biketova111 <89241781930@mail.ru>
2023-01-27 13:33:44 +0900

    Add tack4

---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Напишите программу, которая на вход принимает одно число (N), а на выход показывает в се числа в промежутке от -N  до N.
+Например:
+4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" */
+
+Console.Write("Введите число N: ");
+string namberStr=Console.ReadLine();
+int namber=Math.Abs(Convert.ToInt32(namberStr));
+
+for (int i = -namber; i <= namber; i++)
+{
+    Console.Write($"{i}, ");
+}
