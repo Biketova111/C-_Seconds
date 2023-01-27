bf302510a4e3b630170879f699fae7d34f94bed7
Biketova111 <89241781930@mail.ru>
2023-01-27 13:42:36 +0900

    Add tack5

---
+++ Task5/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Напишите программу, которая на вход принимает трехзначное число и на выходе показывает последнюю цифру этого числа.
+Например:
+125 -> 5 */
+
+Console.Write("Введите трехзначное число: ");
+string numberSrt = Console.ReadLine();
+int namber = Convert.ToInt32(numberSrt);
+
+int result = Math.Abs(namber%10);
+
+Console.WriteLine(result);
+
